da59ebf550e65839ad702a38d65b5a1cc95b02e6
Monica Howard <jay.m.howard@gmail.com>
2017-11-25 22:15:39 -0600

    working on js

--- drumkit/script.js
+++ drumkit/script.js
@@ -1,4 +1,12 @@
 function playSound(e) {
   var audio = document.querySelector(`audio[data-key="${e.keycode}"]`);
   var key = document.querySelector(`div[data]`)
+  if (!audio) return;
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
 }
+
+var keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitioned', removeTransition));
